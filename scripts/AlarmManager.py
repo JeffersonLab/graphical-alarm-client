a5330ae6dbf7470c97d29d5d1d5c463089f9c71c
Michele Joyce <erb@Micheles-MacBook-Pro.local>
2021-02-24 15:13:49 -0500

    ready for show and tell

--- scripts/AlarmManager.py
+++ scripts/AlarmManager.py
@@ -8,7 +8,34 @@ from PyQt5.QtCore import Qt, QObject,QThreadPool
 from PyQt5.QtWidgets import QAction, QToolBar, QSpacerItem, QDialog,QMenu
 
 from JAWSManager import *
+from AlarmShelving import *
 
+class AlarmToolBar(ToolBar) :
+   def __init__(self,parent,*args,**kwargs) :
+      super(AlarmToolBar,self).__init__(parent,*args,**kwargs)
+     
+      shelfaction = ShelfAction(self)
+      self.addAction(shelfaction)
+   
+class AlarmMenuBar(QtWidgets.QMenuBar) :
+   def __init__(self,parent,*args,**kwargs) :
+      
+      super(AlarmMenuBar,self).__init__(parent,*args,**kwargs)          
+      filemenu = self.addMenu("File")
+      
+      shelfaction = QAction("Launch Shelf Manager",self)
+      shelfaction.triggered.connect(self.launchShelfManager)
+      filemenu.addAction(shelfaction)
+      
+      exitaction = QAction("Quit",self)
+      exitaction.triggered.connect(parent.closeEvent)
+      filemenu.addAction(exitaction)
+   
+   
+   def launchShelfManager(self,event) :
+      command = "python3 " + SOURCEDIR + "ShelfManager.py &"
+      os.system(command)
+      
 class AlarmManager(JAWSManager) :
    def __init__(self,*args,**kwargs) :
       super(AlarmManager,self).__init__("JAWS - Active Alarm Manager","alarm",
@@ -17,7 +44,11 @@ class AlarmManager(JAWSManager) :
       self.tableview.setColumnWidth(2,200)
       self.tableview.setColumnWidth(3,150)
       
-      
+   
+   def ToolBar(self) :
+      toolbar = AlarmToolBar(self)
+      return(toolbar)
+         
    def ProxyModel(self) :
       proxymodel = ProxyModel()
       
@@ -32,11 +63,23 @@ class AlarmManager(JAWSManager) :
       modelview = AlarmModel(self.data) 
       
       return(modelview)
-   
+
+
+   def MenuBar(self) :
+      menubar = AlarmMenuBar(self)
+      return(menubar)
+
    def StartProcessor(self) :
       self.processor = AlarmProcessor()
-      print("STARTPROCESSOR:",self.processor)
       
+   def FilterDialog(self) :
+      filterdialog = AlarmFilterDialog()
+      return(filterdialog)
+  
+   def ShelfDialog(self) :
+      shelfdialog = ShelfDialog()
+      return(shelfdialog)
+   
 app = QtWidgets.QApplication(sys.argv)
 window = AlarmManager()
 
--- scripts/ShelfManager.py
+++ scripts/ShelfManager.py
@@ -7,17 +7,47 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt, QObject,QThreadPool
 from PyQt5.QtWidgets import QAction, QToolBar, QSpacerItem, QDialog,QMenu
 
-from JAWSManager import *
+from ShelfProcessor import *
+
+class ShelfMenuBar(QtWidgets.QMenuBar) :
+   def __init__(self,parent,*args,**kwargs) :
+      
+      super(ShelfMenuBar,self).__init__(parent,*args,**kwargs)          
+      filemenu = self.addMenu("File")
+      
+      shelfaction = QAction("Launch Active Alarm Manager",self)
+      shelfaction.triggered.connect(self.launchAlarmManager)
+      filemenu.addAction(shelfaction)
+      
+      exitaction = QAction("Quit",self)
+      exitaction.triggered.connect(parent.closeEvent)
+      filemenu.addAction(exitaction)
+
+   def launchAlarmManager(self,event) :
+      command = "python3 " + SOURCEDIR + "AlarmManager.py &"
+      os.system(command)
+      
+
+class ShelfToolBar(ToolBar) :
+   def __init__(self,parent,*args,**kwargs) :
+      super(ShelfToolBar,self).__init__(parent,*args,**kwargs)
+     
+      unshelfaction = UnShelveAction(self)
+      self.addAction(unshelfaction)
 
 class ShelfManager(JAWSManager) :
    def __init__(self,*args,**kwargs) :
+      self.threadlist = []
+      self.workerlist = []
+      self.shelfqueue = []
+     
       super(ShelfManager,self).__init__("JAWS - Alarm Shelf","shelf",
          *args,**kwargs)
      
-  #    self.tableview.setColumnWidth(2,200)
-   #   self.tableview.setColumnWidth(3,150)
-      
-      
+      self.tableview.setColumnWidth(2,150)
+      self.tableview.setColumnWidth(3,150)
+      self.tableview.setColumnWidth(6,250)
+            
    def ProxyModel(self) :
       proxymodel = ProxyModel()
       
@@ -25,16 +55,96 @@ class ShelfManager(JAWSManager) :
    
    def TableView(self) :
       tableview = ShelfTable()
-     # tableview.sortByColumn(3,1)
+      tableview.sortByColumn(2,1)
       return(tableview)
    
    def ModelView(self) :
       modelview = ShelfModel(self.data)       
       return(modelview)
+   
+   def FilterDialog(self) :
+      filterdialog = ShelfFilterDialog()
+      return(filterdialog)
+   
+   def MenuBar(self) :
+      menubar = ShelfMenuBar(self)
+      return(menubar)
+
       
+   def ToolBar(self) :
+      toolbar = ShelfToolBar(self)
+      return(toolbar)
+
    def StartProcessor(self) :
       self.processor = ShelfProcessor()
+      self.EmptyQueue()
+    
+   #Creates the expiration countdown thread   
+   def StartCountDown(self,alarm) :
+      print(alarm.GetName(),"counting",alarm.counting)
+      thread = QThreadPool()      
       
+      worker = Worker(alarm.CalcTimeLeft,alarm.delay)
+     
+      worker.signals.output.connect(self.CountDown)
+ 
+      #alarm.thread = thread
+      alarm.worker = worker
+      thread.start(worker)
+      if (not thread in self.threadlist) :
+         self.threadlist.append(thread)
+      if (not worker in self.workerlist) :
+         self.workerlist.append(worker)
+  
+ 
+         
+   def CountDown(self,alarm) :      
+      
+     
+      if (not alarm in GetModel().data) :
+        
+         timeleft = None
+      else :         
+         timeleft = alarm.timeleft
+        
+      if (timeleft != None) :
+            
+         row = GetModel().data.index(alarm)
+         index = GetModel().createIndex(row,0)
+         GetModel().dataChanged.emit(index,index,[Qt.DisplayRole])
+         
+      else :
+         worker = alarm.worker
+         worker.stop()
+         alarm.worker = None
+   
+   def stopWorker(self) :
+      super().stopWorker()
+      try :
+         for worker in self.workerlist :           
+            worker.stop()
+         for thread in self.threadlist :
+            thread.join()
+      except :
+         pass
+   
+   
+   def EmptyQueue(self) :
+          
+      for alarm in self.shelfqueue :
+         if (alarm.counting or alarm.GetShelfExpiration() == None) :
+            return
+         worker = None
+         alarm.counting = True
+         self.StartCountDown(alarm)
+               
+   def AddToQueue(self,alarm) :
+      if (alarm.GetShelfExpiration() == None) :
+         return
+      if (not alarm in self.shelfqueue) :
+         self.shelfqueue.append(alarm)  
+
+
 app = QtWidgets.QApplication(sys.argv)
 window = ShelfManager()
 
